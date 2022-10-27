2edacf68c7ef2be0896e34e5e24b1bc53f935c2b
Satrim <59724286+Satrim@users.noreply.github.com>
2022-10-27 15:50:34 +1200

    1.1. Состояние нити. [#229175 #355069]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -8,18 +8,15 @@ public class ThreadState {
         Thread second = new Thread(
                 () -> { }
         );
-        System.out.println("first - " + first.getState());
-
+        System.out.println(first.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
             System.out.println("first - " + first.getState());
-        }
-        System.out.println("first - " + first.getState());
-        System.out.println("second - " + second.getState());
-        while (second.getState() != Thread.State.TERMINATED) {
             System.out.println("second - " + second.getState());
         }
+        System.out.println("first - " + first.getState());
         System.out.println("second - " + second.getState());
     }
 }
