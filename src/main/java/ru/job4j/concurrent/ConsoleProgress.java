ae2fc6226158d11c29ba466424c7ca1011162c4a
Satrim <59724286+Satrim@users.noreply.github.com>
2022-10-29 15:47:25 +1200

    3. Прерывание нити [#1019]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,10 +1,13 @@
 package ru.job4j.concurrent;
 
 public class ConsoleProgress implements Runnable {
+
+    public static final int SIZE = 3;
+
     public static void main(String[] args) throws InterruptedException {
         Thread progress = new Thread(new ConsoleProgress());
         progress.start();
-        Thread.sleep(1000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        Thread.sleep(1000);
         progress.interrupt();
     }
 
@@ -13,7 +16,7 @@ public class ConsoleProgress implements Runnable {
         String[] process = {"\\", "|", "/"};
         int index = 0;
         while (!Thread.currentThread().isInterrupted()) {
-            if (index == 3) {
+            if (index == SIZE) {
                 index = 0;
             }
             System.out.print("\rload: " + process[index++]);
