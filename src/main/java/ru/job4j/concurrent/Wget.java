0428b6fb953c54af592cd8ed05dcf0fcf09d9a30
Satrim <59724286+Satrim@users.noreply.github.com>
2022-10-31 05:10:40 +1200

    4. Скачивание файла с ограничением. [#144271 #356048]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -8,6 +8,8 @@ import java.time.Duration;
 import java.time.Instant;
 
 public class Wget implements Runnable {
+
+    private static final int SEC = 1000;
     private final String url;
     private final int speed;
 
@@ -18,32 +20,46 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        try {
-            Instant startTime = Instant.now();
-            BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-            Instant finishTime = Instant.now();
-            FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml");
-            long downloadTime = Duration.between(startTime, finishTime).toMillis();
+        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(parser(url))) {
+
             byte[] dataBuffer = new byte[speed];
             int bytesRead;
+            int downloadData = 0;
+            Instant start = Instant.now();
+            long downloadTime;
+
             while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                try {
-                    if (downloadTime < speed) {
-                        Thread.sleep(speed - downloadTime);
+                downloadData += bytesRead;
+                if (downloadData >= speed) {
+                    Instant finish = Instant.now();
+                    downloadTime = Duration.between(start, finish).toMillis();
+                    if (downloadTime < SEC) {
+                        try {
+                            Thread.sleep(SEC - downloadTime);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        start = Instant.now();
+                        downloadData = 0;
                     }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
                 }
             }
-            in.close();
-            fileOutputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    private String parser(String str) {
+        String[] parsPath = str.split("/");
+        return parsPath[parsPath.length - 1];
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException();
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
