e090827a2541b6d7150c67683a2cc87651bbdada
Alexander Slepchenko <asander@list.ru>
2022-11-21 15:45:53 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public final class ParseFile {
+
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (OutputStream o = new FileOutputStream(file)) {
+            for (int i = 0; i < content.length(); i += 1) {
+                o.write(content.charAt(i));
+            }
+        }
+    }
+}
