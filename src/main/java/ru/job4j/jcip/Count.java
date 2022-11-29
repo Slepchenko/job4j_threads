054c82799cafcda43ec0318f7c44ab1a82c4985c
Alexander Slepchenko <asander@list.ru>
2022-11-29 19:49:10 +0300

    2. JCIP. Настройка библиотеки [#268575]

---
+++ src/main/java/ru/job4j/jcip/Count.java
@@ -0,0 +1,13 @@
+package ru.job4j.jcip;
+
+public class Count {
+    private int value;
+
+    public void increment() {
+        this.value++;
+    }
+
+    public int get() {
+        return this.value;
+    }
+}
