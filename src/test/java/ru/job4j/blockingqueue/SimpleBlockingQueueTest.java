7f9e5ea963c85c01705af9768ad30d7e8d19afc2
Alexander Slepchenko <asander@list.ru>
2023-02-04 21:02:29 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/blockingqueue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/blockingqueue/SimpleBlockingQueueTest.java
@@ -1,19 +1,48 @@
 package ru.job4j.blockingqueue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
 class SimpleBlockingQueueTest {
-
     @Test
-    public void whenEmptyQueue() {
-
-    }
-
-    @Test
-    public void whenFullQueue() {
-
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(i -> {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    Thread.State state = Thread.currentThread().getState();
+                    while (state.equals(Thread.State.WAITING) || !Thread.currentThread().isInterrupted()) {
+                        state = Thread.currentThread().getState();
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        List<Integer> expect = List.of(0, 1, 2, 3, 4);
+        Assertions.assertIterableEquals(buffer, expect);
     }
 }
