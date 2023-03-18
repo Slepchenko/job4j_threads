1d68c61360f9b3d27b32723353504310a28b9d16
Alexander Slepchenko <asander@list.ru>
2023-03-18 23:24:48 +0300

    3. ForkJoinPool [#315067]

--- src/main/java/ru/job4j/pool/ParallelFindIndex.java
+++ src/main/java/ru/job4j/pool/ParallelFindIndex.java
@@ -7,13 +7,13 @@ public class ParallelFindIndex<T> extends RecursiveTask<Integer> {
     private final T[] array;
     private final int from;
     private final int to;
-    private final T t;
+    private final T obj;
 
-    public ParallelFindIndex(T t, T[] array, int from, int to) {
+    public ParallelFindIndex(T obj, T[] array, int from, int to) {
         this.array = array;
         this.from = from;
         this.to = to;
-        this.t = t;
+        this.obj = obj;
     }
 
     @Override
@@ -22,15 +22,17 @@ public class ParallelFindIndex<T> extends RecursiveTask<Integer> {
             return from;
         }
         int mid = (from + to) / 2;
-        ParallelFindIndex<T> leftFind = new ParallelFindIndex<>(t, array, from, mid);
-        ParallelFindIndex<T> rightFind = new ParallelFindIndex<>(t, array, mid + 1, to);
-        leftFind.fork();
-        rightFind.fork();
-        int l = leftFind.join();
-        int r = rightFind.join();
-        if (array[l].equals(t)) {
-            return l;
+        ParallelFindIndex<T> left = new ParallelFindIndex<>(obj, array, from, mid);
+        ParallelFindIndex<T> right = new ParallelFindIndex<>(obj, array, mid + 1, to);
+        left.fork();
+        right.fork();
+        return result(left.join(), right.join());
+    }
+
+    private int result(int l, int r) {
+        if (array[r].equals(obj)) {
+            return r;
         }
-        return r;
+        return l;
     }
 }
