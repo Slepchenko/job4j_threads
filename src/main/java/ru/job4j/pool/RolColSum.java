d1d5bd0a07f933fcf13c4af2033702a436d32e0d
Alexander Slepchenko <asander@list.ru>
2023-03-27 22:33:12 +0300

    4. CompletableFuture [#361626]

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -5,26 +5,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
-    }
 
     public static Sums[] sum(int[][] matrix) {
         Sums[] res = new Sums[matrix.length];
@@ -56,6 +36,28 @@ public class RolColSum {
         return res.get();
     }
 
+    private static CompletableFuture<Sums> getData(int[][] matrix, int index) {
+        Sums sums = new Sums();
+
+        return CompletableFuture.supplyAsync(() -> {
+            int row = 0;
+            int col = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (i == index) {
+                        row += matrix[i][j];
+                    }
+                    if (j == index) {
+                        col += matrix[i][j];
+                    }
+                }
+            }
+            sums.setRowSum(row);
+            sums.setColSum(col);
+            return sums;
+        });
+    }
+
     private static CompletableFuture<int[]> getRow(int[][] data) {
         return CompletableFuture.supplyAsync(() -> {
             int[] res = new int[data.length];
@@ -81,13 +83,19 @@ public class RolColSum {
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        int[][] arr = new int[10000][10000];
-        for (int[] ints : arr) {
-            Arrays.fill(ints, 1);
-        }
-        long start = System.currentTimeMillis();
-        RolColSum.asyncSum(arr);
-        long finish = System.currentTimeMillis();
-        System.out.println(finish - start);
+//        int[][] arr = new int[10000][10000];
+//        for (int[] ints : arr) {
+//            Arrays.fill(ints, 1);
+//        }
+//        long start = System.currentTimeMillis();
+//        RolColSum.asyncSum(arr);
+//        long finish = System.currentTimeMillis();
+//        System.out.println(finish - start);
+
+        int[][] arr = new int[][]{{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+
+        System.out.println(getData(arr, 1).get().getRowSum() + " " + getData(arr, 1).get().getColSum());
     }
 }
